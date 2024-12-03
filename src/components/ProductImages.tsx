5b4c94573f404448b000a8fe2f74a8d09c474b9b
Nazik-0 <kizan5035@gmail.com>
2024-12-03 20:11:27 +0530

    commit changes

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -9,20 +9,21 @@ const images = [
      },
      {
        id: 2,
-       url: "https://images.pexels.com/photos/17867705/pexels-photo-17867705/free-photo-of-crowd-of-hikers-on-the-mountain-ridge-at-dusk.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+       url: "https://images.pexels.com/photos/3772506/pexels-photo-3772506.jpeg?auto=compress&cs=tinysrgb&w=600",
      },
       {
         id: 3,
-        url: "https://images.pexels.com/photos/21812160/pexels-photo-21812160/free-photo-of-puerta-colonial-color-rojo-de-guanajuato-mexico.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+        url: "https://images.pexels.com/photos/1082526/pexels-photo-1082526.jpeg?auto=compress&cs=tinysrgb&w=600",
      },
       {
         id: 4,
-        url: "https://images.pexels.com/photos/20832069/pexels-photo-20832069/free-photo-of-a-narrow-street-with-buildings-and-cars.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+        url: "https://images.pexels.com/photos/2723623/pexels-photo-2723623.jpeg?auto=compress&cs=tinysrgb&w=600",
       },
      ];
 
 const ProductImages = () => {
-    const [index, setIndex] = useState(0);
+
+    const [index,setIndex] = useState(0)
     return (
       <div className="">
           <div className="h-[500px] relative">
@@ -36,7 +37,7 @@ const ProductImages = () => {
           </div>
           <div className="flex justify-between gap-4 mt-8">
              {images.map((img,i) => (
-              <div className="w-1/4 h-32 relative gap-4 mt-8" key={img.id} onClick={() => setIndex(i)}>
+              <div className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer" key={img.id} onClick={() => setIndex(i)}>
               <Image 
                 src={img.url}
                 alt=""
